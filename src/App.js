f7a87ebcae359250d133c5aad1b71faa09a16e4c
Matthew Mbonu <mcmbonu1@gmail.com>
2019-08-01 12:33:11 -0400

    finished

--- src/App.js
+++ src/App.js
@@ -1,56 +1,87 @@
-import React, { Component } from 'react';
-import List from './List'
-import './App.css';
+import React, { Component } from "react";
+import List from "./List";
+import "./App.css";
 
 class App extends Component {
-  static defaultProps = {
-    store: {
-      lists: [],
-      allCards: {},
-    }
-  };
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       lists: this.props.store.lists,
-      allCards: this.props.store.allCards,
-    }
+      allCards: this.props.store.allCards
+    };
   }
 
   omit(obj, keyToOmit) {
     return Object.entries(obj).reduce(
       (newObj, [key, value]) =>
-          key === keyToOmit ? newObj : {...newObj, [key]: value},
+        key === keyToOmit ? newObj : { ...newObj, [key]: value },
       {}
     );
   }
 
-  handleDelete = (id) => {
-    const list = this.state.lists.map(obj => obj.cardIds.filter(cardId => cardId !== id));
-    console.log(list); 
+  handleDelete = id => {
+    const list = this.state.lists.map(obj => ({
+      ...obj,
+      cardIds: obj.cardIds.filter(card => card !== id)
+    }));
+    console.log(list);
     this.setState({
       allCards: this.omit(this.state.allCards, id),
       lists: list
-    })
-  }
+    });
+  };
 
-  addRandomCard = () => {}
+  newRandomCard = () => {
+    const id =
+      Math.random()
+        .toString(36)
+        .substring(2, 4) +
+      Math.random()
+        .toString(36)
+        .substring(2, 4);
+    return {
+      id,
+      title: `Random Card ${id}`,
+      content: "lorem ipsum"
+    };
+  };
+
+  handleRandom = listId => {
+    const randomCard = this.newRandomCard();
+    const list = this.state.lists.map(obj => {
+      if (obj.id === listId) {
+        console.log(obj);
+        return {
+          ...obj,
+          cardIds: [...obj.cardIds, randomCard.id]
+        };
+      } else {
+        return obj;
+      }
+    });
+    this.setState({
+      lists: list,
+      allCards: { ...this.state.allCards, [randomCard.id]: randomCard }
+    });
+  };
 
   render() {
-    const { store } = this.props
+    const { lists, allCards } = this.state;
+    console.log(lists, allCards);
     return (
-      <main className='App'>
-        <header className='App-header'>
+      <main className="App">
+        <header className="App-header">
           <h1>Trelloyes!</h1>
         </header>
-        <div className='App-list'>
-          {store.lists.map(list => (
+        <div className="App-list">
+          {lists.map(list => (
             <List
               key={list.id}
+              id={list.id}
               header={list.header}
-              cards={list.cardIds.map(id => store.allCards[id])}
+              cards={list.cardIds.map(id => allCards[id])}
               handleDelete={this.handleDelete}
+              handleRandom={this.handleRandom}
             />
           ))}
         </div>
--- src/List.js
+++ src/List.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import Card from './Card'
-import './List.css';
+import React from "react";
+import Card from "./Card";
+import "./List.css";
 
 export default function List(props) {
   return (
-    <section className='List'>
-      <header className='List-header'>
+    <section className="List">
+      <header className="List-header">
         <h2>{props.header}</h2>
       </header>
-      <div className='List-cards'>
-        {props.cards.map((card) =>
+      <div className="List-cards">
+        {props.cards.map(card => (
           <Card
             key={card.id}
             id={card.id}
@@ -17,14 +17,15 @@ export default function List(props) {
             content={card.content}
             handleDelete={props.handleDelete}
           />
-        )}
+        ))}
         <button
-          type='button'
-          className='List-add-button'
+          type="button"
+          className="List-add-button"
+          onClick={() => props.handleRandom(props.id)}
         >
           + Add Random Card
         </button>
       </div>
     </section>
-  )
+  );
 }
